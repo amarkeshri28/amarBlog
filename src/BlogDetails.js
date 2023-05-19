c66f4015aac787021e6982a65867f221c15f5c73
amarkeshri28 <amarjeetkeshri2013@gmil.com>
2023-05-20 02:00:42 +0530

    added delete button

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,17 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
 
     const { id } = useParams();
     const { data: blog, isPending, error } = useFetch("http://localhost:8000/blogs/" + id);
-
+    const history = useHistory();
+    const handleDelete = () => {
+        fetch('http://localhost:8000/blogs/' + id, {
+            method: 'DELETE'
+        }).then(() => history.push('/')
+        )
+    }
     return (
         <div className="blog-details">
             {isPending && <div> Loading....</div>}
@@ -17,6 +23,7 @@ const BlogDetails = () => {
                     <div>
                         {blog.body}
                     </div>
+                    <button onClick={handleDelete}>Delete</button>
                 </article>
             )}
         </div>
