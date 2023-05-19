87316a022c9005c79cb4f286696482cb7ce8bdc3
amarkeshri28 <amarjeetkeshri2013@gmil.com>
2023-05-20 01:50:01 +0530

    added moving back to home page

--- src/Create.js
+++ src/Create.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 
 const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
     const [isPending, setIsPending] = useState(false);
-
+    const history = useHistory();
     const handleSubmit = (e) => {
         e.preventDefault();
         setIsPending(true);
@@ -16,7 +18,8 @@ const Create = () => {
             body: JSON.stringify(blog)
         }).then(() => {
             setIsPending(false);
-        })
+            history.push('/');
+        });
     }
 
     return (
